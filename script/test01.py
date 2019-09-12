4466a193cb48ca56b74c87605a6e7bad9ce12b1a
tangjing <tangjing_work@163.com>
2019-09-12 16:13:33 +0800

    001

---
+++ script/test01.py
@@ -0,0 +1,10 @@
+class Test01:
+    def test01(self):
+        print( '这是第一个' )
+
+    def test02(self):
+        print( '这是第二个' )
+
+    def test03(self):
+        print( '这是第三个' )
+        assert 1 == 2
